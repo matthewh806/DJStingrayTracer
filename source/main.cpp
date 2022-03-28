0f22546fdb891f39357c8a1995f072991b8a74d9
matthew <harris@ircam.fr>
2022-03-29 01:02:07 +0200

    add most basic surface normal

--- source/main.cpp
+++ source/main.cpp
@@ -3,23 +3,26 @@
 #include "colour.hpp"
 #include "ray.hpp"
 
-bool hitSphere(const point3& center, double radius, const Ray& r)
+double hitSphere(const point3& center, double radius, const Ray& r)
 {
     Vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius * radius;
-    return b*b - 4*a*c > 0;
+    const auto a = dot(r.direction(), r.direction());
+    const auto b = 2 * dot(r.direction(), oc);
+    const auto c = dot(oc, oc) - radius * radius;
+    const auto discriminant = b*b - 4*a*c;
+    return discriminant < 0.0 ? -1.0 : (-b - std::sqrt(discriminant)) / (2.0*a);
 }
 
 colour rayColour(const Ray &ray)
 {
-    if(hitSphere(point3(0.0, 0.0, -1.0), 0.5, ray))
+    auto t = hitSphere(point3(0.0, 0.0, -1.0), 0.5, ray);
+    if(t > 0.0)
     {
-        return colour(1.0, 0.0, 0.0);
+        Vec3 n = unitVector(ray.at(t) - Vec3(0.0, 0.0, -1.0));
+        return 0.5 * colour(n.x() + 1, n.y() + 1, n.z() + 1);
     }
     const Vec3 unitDirection = unitVector(ray.direction());
-    const auto t = 0.5 * (unitDirection.y() + 1.0);
+    t = 0.5 * (unitDirection.y() + 1.0);
     return (1.0 - t) * colour(1.0, 1.0, 1.0) + t * colour(0.5, 0.7, 1.0);
 }
 
