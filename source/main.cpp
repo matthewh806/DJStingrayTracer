0382ffceaf614dd15bcf6d235bcf514ef1cabd58
matthewh806 <matthew.harris@ircam.fr>
2022-03-30 00:24:06 +0200

    Update main fn with json world input data

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include "json.hpp"
+#include "cxxopts.hpp"
 #include "vec3.hpp"
 #include "colour.hpp"
 #include "ray.hpp"
@@ -7,37 +10,38 @@
 
 const double infinity = std::numeric_limits<double>::infinity();
 
-colour rayColour(const Ray &ray, const Hittable& world)
+colour rayColour(const Ray &ray, const Hittable& world, colour colourFrom, colour colourTo)
 {
     HitRecord hitRecord;
     if(world.hit(ray, 0.0, infinity, hitRecord))
     {
-        return 0.5 * (hitRecord.normal + colour(1.0, 1.0, 1.0));
+        return 0.5 * (hitRecord.normal + colourFrom);
     }
     
     const Vec3 unitDirection = unitVector(ray.direction());
     const auto t = 0.5 * (unitDirection.y() + 1.0);
-    return (1.0 - t) * colour(1.0, 1.0, 1.0) + t * colour(0.5, 0.7, 1.0);
+    return (1.0 - t) * colourFrom + t * colourTo;
 }
 
-int main()
+void initialiseWorld(const int width = 400, const point3& origin = point3(0.0, 0.0, 0.0), const colour& colourFrom = colour(1.0, 1.0, 1.0), const colour& colourTo = colour(0.5, 0.7, 1.0), std::vector<std::shared_ptr<Hittable>> hittables = std::vector<std::shared_ptr<Hittable>>())
 {
     // Image
     const auto aspectRatio = 16.0 / 9.0;
-    const auto imageWidth = 400;
+    const auto imageWidth = width;
     const auto imageHeight = static_cast<int>(imageWidth / aspectRatio);
     
     // World
     HittableList world;
-    world.add(std::make_shared<Sphere>(point3(0.0, 0.0, -1.0), 0.5));
-    world.add(std::make_shared<Sphere>(point3(0.0, -100.5, -1.0), 100.0));
+    for(auto& hittable : hittables)
+    {
+        world.add(std::move(hittable));
+    }
     
     // Camera
     const auto viewportHeight = 2.0;
     const auto viewportWidth = aspectRatio * viewportHeight;
     const auto focalLength = 1.0;
-    
-    const auto origin = point3(0.0, 0.0, 0.0);
+
     const auto horizontal = Vec3(viewportWidth, 0.0, 0.0);
     const auto vertical = Vec3(0.0, viewportHeight, 0.0);
     const auto lowerLeftCorner = origin - horizontal / 2 -  vertical / 2 - Vec3(0.0, 0.0, focalLength);
@@ -53,10 +57,55 @@ int main()
             auto const u = static_cast<double>(i) / (imageWidth - 1);
             auto const v = static_cast<double>(j) / (imageHeight - 1);
             Ray r(origin, lowerLeftCorner + u * horizontal + v * vertical - origin);
-            colour pixelColour = rayColour(r, world);
+            colour pixelColour = rayColour(r, world, colourFrom, colourTo);
             writeColour(std::cout, pixelColour);
         }
     }
     
     std::cerr << "\nDone.\n";
+}
+
+int main(int argc, char** argv)
+{
+    cxxopts::Options options("WeekendRayTracer", "A simple raytracer");
+    options.add_options()
+    ("i, inputdata", "path to input json data", cxxopts::value<std::string>());
+    
+    auto result = options.parse(argc, argv);
+    
+    std::string jsonPath = "";
+    if(result.count("inputdata"))
+    {
+        jsonPath = result["inputdata"].as<std::string>();
+    }
+    
+    if(jsonPath.empty())
+    {
+        std::cerr << "No json path specified\n";
+        return 1;
+    }
+    
+    std::ifstream inputStream(jsonPath);
+    auto const inputDataJson = nlohmann::json::parse(inputStream);
+    
+    auto const convertVectorToVec3 = [](const std::vector<double>& vector) -> Vec3
+    {
+        return Vec3(vector[0], vector[1], vector[2]);
+    };
+    
+    auto const width = inputDataJson.at("width").get<int>();
+    auto const origin = convertVectorToVec3(inputDataJson.at("origin").get<std::vector<double>>());
+    auto const colourFromVec = convertVectorToVec3(inputDataJson.at("colourfrom").get<std::vector<double>>());
+    auto const colourToVec = convertVectorToVec3(inputDataJson.at("colourto").get<std::vector<double>>());
+    auto const hittablesJson = inputDataJson.at("hittables");
+    std::vector<std::shared_ptr<Hittable>> hittables;
+    
+    for(auto const& hittable : hittablesJson)
+    {
+        auto const pos = convertVectorToVec3(hittable.at("position").get<std::vector<double>>());
+        auto const radius = hittable.at("radius").get<double>();
+        hittables.push_back(std::make_shared<Sphere>(pos, radius));
+    }
+    
+    initialiseWorld(width, origin, colourFromVec, colourToVec, hittables);
 }
