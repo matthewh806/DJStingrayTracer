d9d5fd0ec051c943654b6b2fc09e681042490b24
matthewh806 <matthew.harris@ircam.fr>
2022-03-31 16:29:15 +0200

    Add ranged random double method to utils

--- source/utils.hpp
+++ source/utils.hpp
@@ -7,9 +7,14 @@
 // Constants
 
 // Utility Functions
-inline double randomDouble()
+inline double randomDouble(double min, double max)
 {
-    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::uniform_real_distribution<double> distribution(min, max);
     static std::mt19937 generator;
     return distribution(generator);
+}
+
+inline double randomDouble()
+{
+    randomDouble(0.0, 1.0);
 }
